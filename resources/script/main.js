b2c2dd9b7be145390810d688e07ff64783e25809
CamilaNepomuceno <38860566+CamilaNepomuceno@users.noreply.github.com>
2018-05-09 15:47:58 +0100

    JavaDay2

---
+++ resources/script/main.js
@@ -0,0 +1,78 @@
+//window.alert("testing to see if linked");
+
+//console.log("testing to see if linked");
+
+
+//var is the declaration of a variable
+
+//"name" is what we have decided to call it
+var name = "Max";
+var job;
+var salary;
+// We can criate 2 variable on the same line, it works the same 
+//var name = "Max", job:
+// variable is a container that stores data
+
+job = "Developer";
+salary = 25000;
+
+// Data types for javaScript - numbers, strings, boolean, undefined and null
+
+//console.log(job);
+
+// var 1student; // cannot start name with a number
+//var _1student; // underscore can be added before the number
+
+//var student; // javaScript is case sensitive
+//var Student; 
+
+// window.alert("Hello World");
+
+// document.getElementById("demo").innerHTML = "Hello World!";
+
+// Declaring a var to hold the input
+
+//var event1 = document.getElementById("button1");
+
+// setup an eventlistener on button1 Click 
+
+//event1.addEventListener('click',functionName);
+
+// function functionName( ){
+	 
+	// tested to see if  its working
+	//window.alert("It is working !!!! ");
+
+// Storing the information entered by the user
+//var userEntered  = document.getElementById("enter").value;
+
+//document.getElementById("output").innerHTML = userEntered;
+//}
+
+
+
+var number1;
+	var number2;
+	var result;
+
+function sumNumbers() {
+	
+	var number1 = document.getElementById("enter1").value;
+	var number2 = document.getElementById("enter2").value;
+	var result = number1+number2;
+	
+	document.getElementById("output").innerHTML = result;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
